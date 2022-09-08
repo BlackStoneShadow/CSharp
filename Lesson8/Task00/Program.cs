62b981d1019b6228353fdf163b8195b4b68ee0ce
Tolik <ProtorskyAV@yandex.ru>
2022-09-08 21:07:05 +0300

    Modify home task Lesson8

---
+++ Lesson8/Task00/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Напишите программу, которая будет принимать на вход //число и возвращать сумму его цифр
+namespace Task00
+{
+    class Program
+    {
+        static int ReadNumber(string Caption)
+        {
+            while (true)
+            {
+                Console.Write($"{Caption}:");
+
+                if (Int32.TryParse(Console.ReadLine(), out int Result))
+                {
+                    return Result;
+                }
+                else
+                {
+                    Console.WriteLine("Error input!");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int Number = ReadNumber("Input number");
+
+            int Sum = 0;
+            while(Number > 0)
+            {
+                Sum += Number % 10; Number /= 10;
+            }
+
+            Console.WriteLine($"Sum={Sum}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson8/Task01/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Напишите программу, которая на вход принимает два числа A и B, //и возводит число А в целую степень B с помощью рекурсии.
+namespace Task01
+{
+    class Program
+    {
+        static int ReadNumber(string Caption)
+        {
+            while (true)
+            {
+                Console.Write($"{Caption}:");
+
+                if (Int32.TryParse(Console.ReadLine(), out int Result))
+                {
+                    return Result;
+                }
+                else
+                {
+                    Console.WriteLine("Error input!");
+                }
+            }
+        }
+        static int Power(int A, int B)
+        {
+            if (B > 1)
+            {
+                return A * Power(A, B - 1);
+            }
+            else
+            {
+                return A;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int A = ReadNumber("Input A");
+            int B = ReadNumber("Input B");
+
+            Console.WriteLine($"Power={Power(A, B)}");
+
+            Console.ReadKey();
+        }
+    }
+}
